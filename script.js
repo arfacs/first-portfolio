5ed4e114cfa6d87cbb7695c7bf65caa285270258
keerthana <keertananaik86@gmail.com>
2024-11-13 15:46:24 +0530

    Simple portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+function showDetails(projectId) {
+    var details = document.getElementById(projectId);
+    if (details.style.display === "none" || details.style.display === "") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
